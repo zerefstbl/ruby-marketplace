a06dcf724df7114113e58d5a44c75c160b906830
Eduardo <duduoli11@hotmail.com>
2024-06-01 23:36:11 -0300

    (feat): Added routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# rubocop:disable Layout/HashAlignment
+# rubocop:disable Layout/ArgumentAlignment
+
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  namespace :api,
-            defaults: { format: :json },
-            constraints: { subdomain: 'api' },
-            path: '/' do
-    scope module: :v1 do
+  namespace :api, defaults: { format: :json },
+  constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+    constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
 end
