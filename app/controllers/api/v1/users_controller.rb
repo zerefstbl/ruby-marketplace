fb435b7c3eb3bebfaab48330f828a354603fa838
Eduardo <duduoli11@hotmail.com>
2024-06-02 20:20:36 -0300

    (feat): Added route to create new users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,25 @@
 module Api
     module V1
         class UsersController < ApplicationController
-
             def show
                 render json: User.find(params[:id])
             end
 
+            def create
+                user = User.create(user_params)
+
+                if user.save
+                    render json: user, status: 201, localtion: [:api, user]
+                else
+                    render json: { errors: user.errors }, status: 422
+                end
+            end
+
+          private
+
+            def user_params
+                params.require(:user).permit(:email, :password, :password_confirmation)
+            end
         end
     end
 end
