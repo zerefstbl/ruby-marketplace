2019d689cae2ecb1ab3a7a4bc9528683020acfc5
Eduardo <duduoli11@hotmail.com>
2024-06-02 22:21:15 -0300

    (tests): Added tests when user is deleted

--- spec/controllers/api/v1/users_spec.rb
+++ spec/controllers/api/v1/users_spec.rb
@@ -88,6 +88,16 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with 422 }
     end
   end
+
+  describe 'DELETE #destroy' do
+    before(:each) do
+      @user = FactoryBot.create :user
+      delete :destroy, params: { id: @user.id }, format: :json
+    end
+
+    it { should respond_with 204 }
+
+  end
 end
 
 # rubocop:enable Metrics/BlockLength
