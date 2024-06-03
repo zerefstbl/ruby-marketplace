86fe2a5b14a347bdcf3f36363bc0b4809805aebe
Eduardo <duduoli11@hotmail.com>
2024-06-02 22:21:30 -0300

    (feat): Added route to delete user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,12 +15,19 @@ module Api
             end
 
             def update
+
                 user = User.find(params[:id])
                 if user.update(user_params)
                     render json: user, status: 201, location: [:api, user]
                 else
                     render json: { errors: user.errors }, status: 422
                 end
+            end
+
+            def destroy
+                user = User.find(params[:id])
+                user.destroy
+                head 204
             end
 
           private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: %i[show create update]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
