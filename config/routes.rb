de56c9b71ce127733a0d0f2446027eead0956ab9
Eduardo <duduoli11@hotmail.com>
2024-06-02 21:57:49 -0300

    (feat): Added new route and controller to update user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: %i[show create update]
     end
   end
 end
