933a27f5dbb1bb12c8385ed29b7bdf4ce368d8b9
Eduardo <duduoli11@hotmail.com>
2024-06-04 19:08:16 -0300

    (refactor): Refactor module structure'

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,39 +1,36 @@
-module Api
-    module V1
-        class UsersController < ApplicationController
-            def show
-                render json: User.find(params[:id])
-            end
+module Api::V1
+    class UsersController < ApplicationController
+        def show
+            render json: User.find(params[:id])
+        end
 
-            def create
-                user = User.new(user_params)
-                if user.save
-                    render json: user, status: 201, location: [:api, user]
-                else
-                    render json: { errors: user.errors }, status: 422
-                end
+        def create
+            user = User.new(user_params)
+            if user.save
+                render json: user, status: 201, location: [:api, user]
+            else
+                render json: { errors: user.errors }, status: 422
             end
+        end
 
-            def update
-                user = User.find(params[:id])
-                if user.update(user_params)
-                    render json: user, status: 201, location: [:api, user]
-                else
-                    render json: { errors: user.errors }, status: 422
-                end
+        def update
+            user = User.find(params[:id])
+            if user.update(user_params)
+                render json: user, status: 201, location: [:api, user]
+            else
+                render json: { errors: user.errors }, status: 422
             end
+        end
 
-            def destroy
-                user = User.find(params[:id])
-                user.destroy
-                head 204
-            end
+        def destroy
+            User.find(params[:id]).destroy!
+            head 204
+        end
 
-          private
+        private
 
-            def user_params
-                params.require(:user).permit(:email, :password, :password_confirmation)
-            end
+        def user_params
+            params.require(:user).permit(:email, :password, :password_confirmation)
         end
     end
 end
